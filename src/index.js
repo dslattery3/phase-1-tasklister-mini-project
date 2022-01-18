68aa158743cbf2bc9da63e09b7f76a312c1480e3
Dalton <slattery.da@gmail.com>
2022-01-18 12:08:39 -0600

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target['new-task-description'].value)
+  })
 });
+
+function addToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = 'X'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#create-task-form').appendChild(p)
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
+}
+
+// const taskForm = document.getElementById('create-task-form')
+// const input = document.getElementById('new-task-description')
+// const list = document.getElementById('list')
+
